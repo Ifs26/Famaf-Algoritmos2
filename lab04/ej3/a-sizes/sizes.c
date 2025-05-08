8c97fbfc6948c3acdf51d3fc8a5c2ae9a6fcd28e
Luca <31829436+Ifs26@users.noreply.github.com>
2025-05-08 17:33:24 -0300

    agregue parte 3.a.3

--- lab04/ej3/a-sizes/sizes.c
+++ lab04/ej3/a-sizes/sizes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "data.h"
 
@@ -27,19 +28,31 @@ int main(void) {
            "height-size: %lu bytes\n"
            "data_t-size: %lu bytes\n\n", n_size,a_size,h_size,t_size);
 
-            printf("name-size  : %lu \n"
-            "age-size   : %lu\n"
-            "height-size: %lu\n"
-            "data_t-size: %lu\n\n",  (uintptr_t) &messi.name,(uintptr_t) &messi.age,
-            (uintptr_t) &messi.height, (uintptr_t) &messi);
+    printf("name-size  : %lu \n"
+    "age-size   : %lu\n"
+    "height-size: %lu\n"
+    "data_t-size: %lu\n\n",  (uintptr_t) &messi.name,(uintptr_t) &messi.age,
+    (uintptr_t) &messi.height, (uintptr_t) &messi);
 
-            printf("name-size  : %p \n"
-                "age-size   : %p\n"
-                "height-size: %p\n"
-                "data_t-size: %p\n\n",(void *) &messi.name,(void *) &messi.age,
-                (void *) &messi.height,(void* )&messi);
+    printf("name-size  : %p \n"
+        "age-size   : %p\n"
+        "height-size: %p\n"
+        "data_t-size: %p\n\n",(void *) &messi.name,(void *) &messi.age,
+        (void *) &messi.height,(void* )&messi);
 
+    data_t *p;
+    p = malloc(sizeof(data_t));
+    strcpy(p->name,"Leo Messi");
+    p->age = 36;
+    p->height = 169;
 
+    printf("name  : %s bytes\n"
+        "age   : %d bytes\n"
+        "height: %d bytes\n"
+        , p->name,p->age,p->height);
+
+    free(p);
+    p = NULL;
 
     /*
      *
